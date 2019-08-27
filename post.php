6f1555305587141340daa4ad9ed0054a06987a55
GERZAC1002 <gernot.zacharias@gmx.de>
2019-08-27 21:27:51 +0200

    Can now use newline while posting

--- post.php
+++ post.php
@@ -12,9 +12,9 @@ if(isset($_POST["post"])&&isset($_POST["antwort"])&&isset($_POST["thema"])){
 		$datei = fopen($dateiname, "r")or die("Kann Datei nicht öffnen");
 		$inhalt = fread($datei, filesize($dateiname));
 		fclose($datei);
-		//$antwort = implode("<br>",explode("\n",$antwort));
 		$antwort = implode("&lt;",explode("<",$antwort));
 		$antwort = implode("&gt;",explode(">",$antwort));
+		$antwort = implode("<br>",explode("\n",$antwort));
 		$inhalt = substr_replace($inhalt,"<article><p id=\"kopf\"><u><b>&lt;".date("d.m.Y H:i:s")."&gt; Anonymous</b></u></p><p>".$antwort."</p></article>",-2);
 		$datei=fopen($dateiname,"w+") or die("Konnte Datei nicht öffnen");
 		fwrite($datei,$inhalt);
