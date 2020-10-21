3a82f6d21ed9dc0749bda5f4b782a6bb93acce1d
GERZAC1002 <gernot.zacharias@gmx.de>
2020-10-21 22:47:43 +0200

    Fixed bug: post length is determined at the server now

--- post.php
+++ post.php
@@ -3,13 +3,18 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 if(isset($_POST["post"])&&isset($_POST["antwort"])&&isset($_POST["thema"])){
-	if(($_POST["antwort"]=="") and (strlen($_POST["antwort"]) > 20000) and (strlen($_POST["thema"])>1024) and ($_POST["thema"] == "")){
-		header("Location:".$_POST["thema"].".html");
+	if(((strlen($_POST["antwort"]) > 20000) or (strlen($_POST["thema"])>1024))){
+		//header("Location:".$_POST["thema"].".html");
+		header("Location:fehlerseiten/Fehler0.html");
 	}else{
-		if(($_FILES["dateihoch"]["size"] < 5000000) && is_uploaded_file($_FILES['dateihoch']['tmp_name'])){
-			$dateihoch = "./data/".basename($_FILES["dateihoch"]["name"]);
+		if(($_FILES["dateihoch"]["size"] > 0) && ($_FILES["dateihoch"]["size"] < 5000000) && is_uploaded_file($_FILES['dateihoch']['tmp_name'])){
+			$dateimname = basename($_FILES["dateihoch"]["mame"]);
+			$dateiname = implode("_",explode("\\",$dateiname));
+			$dateiname = implode("_",explode("\"",$dateiname));
+			$dateiname = implode("_",explode("\$",$dateiname));
+			$dateihoch = "./data/".$dateiname;
 			$dateityp = strtolower(pathinfo($_FILES["dateihoch"]["name"],PATHINFO_EXTENSION));
-			if($dateityp != "php"){
+			if($dateityp != "php" and $dateityp != ".js"){
 				while(file_exists($dateihoch)){
 					$dateihoch = rand(1,getrandmax()).rand(1,getrandmax()).".".$dateityp;
 				}
@@ -18,28 +23,28 @@ if(isset($_POST["post"])&&isset($_POST["antwort"])&&isset($_POST["thema"])){
 						if($dateityp == "png" || $dateityp == "jpg" || $dateityp == "gif" || $dateityp == "jpeg"){
 							$zusatz = "<p><a target=\"_blank\" href=\"$dateihoch\"><img width=\"100\" height=\"100\" src=\"$dateihoch\"></img></a></p>";
 						}else{
-							$zusatz = "<p><a target=\"_blank\" href=\"$dateihoch\">".$_FILES["dateihoch"]["name"]."</a></p>";
+							$zusatz = "<p><a target=\"_blank\" href=\"$dateihoch\">".$_FILES["dateihoch"]["name"]."</a><br>Dateigröße: ".($_FILES["dateihoch"]["size"]/1000)."KB</p>";
 						}
 						$fehler = 0;
 					}else{
 						$fehler = 1;
-						//$zusatz = "<p id=\"Fehler\" >Datei".$_FILES["dateihoch"]["name"]." konnte nicht kopiert werden!</p>";
+						$zusatz = "<p id=\"Fehler\" >Datei".$_FILES["dateihoch"]["name"]." konnte nicht kopiert werden!</p>";
 					}
 				}else{
 					$fehler = 2;
-					//$zusatz = "<p id=\"Fehler\" >Datei ".$_FILES["dateihoch"]["name"]." existiert schon!</p>";
+					$zusatz = "<p id=\"Fehler\" >Datei ".$_FILES["dateihoch"]["name"]." existiert schon!</p>";
 				}
 			}else{
 				$fehler = 3;
-				//$zusatz = "<p>PHP Datei Upload ist untersagt:".$dateityp."</p>";
+				$zusatz = "<p>PHP Datei Upload ist untersagt:".$dateityp."</p>";
 			}
 		}else{
-			if($_FILES["dateihoch"]["size"] >= 5000000){
+			if(($_FILES["dateihoch"]["size"] >= 5000000) || ($_FILES["dateihoch"]["size"] = 0)){
 				$fehler = 4;
-				//$zusatz = $zusatz."<p>Fehler bei dateigröße:".$_FILES["dateihoch"]["size"]."</p>";
+				$zusatz = $zusatz."<p>Fehler bei dateigröße:".$_FILES["dateihoch"]["size"]."</p>";
 			}
 		}
-		if($fehler == 0){
+		if(($fehler == 0) && (($_FILES["dateihoch"]["size"] > 0) || ($_POST["antwort"] != ""))){
 			if(file_exists("kommentar.txt")){
 				$dateiname = "kommentar.txt";
 				$datei = fopen($dateiname, "r");
@@ -65,14 +70,18 @@ if(isset($_POST["post"])&&isset($_POST["antwort"])&&isset($_POST["thema"])){
 			$antwort = implode("&lt;",explode("<",$antwort));
 			$antwort = implode("&gt;",explode(">",$antwort));
 			$antwort = implode("<br>",explode("\n",$antwort));
-			$inhalt = substr_replace($inhalt,"</article>\n<article><p id=\"kopf\"><u><b>&lt;".date("d.m.Y H:i:s")."&gt; Anonymous &gt;&gt;$nummer</b></u></p><p>".$zusatz."</p><p>".$antwort."</p></article>",-10);
+			$inhalt = substr_replace($inhalt,"</article>\n<article id=\"$nummer\"><p id=\"kopf\"><u><b>&lt;".date("d.m.Y H:i:s")."&gt; Anonymous &gt;&gt;$nummer</b></u></p><p>".$zusatz."</p><p>".$antwort."</p></article>",-10);
 			$datei=fopen($dateiname,"w+") or die("Konnte Datei nicht öffnen");
 			fwrite($datei,$inhalt);
 			fclose($datei);
 			header("Location:".$thema.".html");
 			//echo $inhalt;
 		}else{
-			header("Location:fehlerseiten/Fehler".$fehler.".html");
+			if(fehler == 0){
+				header("Location:".$_POST["thema"].".html");
+			}else{
+				header("Location:fehlerseiten/Fehler".$fehler.".html");
+			}
 		}
 	}
 }else{
